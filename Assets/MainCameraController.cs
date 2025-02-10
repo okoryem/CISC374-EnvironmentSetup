fc3d3e2ed44eb921114bff258a47c4fe7d472e2b
okoryem <okoryem@gmail.com>
2025-02-09 20:22:30 -0800

    Set up. Initial commit

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("CS Joke. I don't know. Me Tired and me brain no worky.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
